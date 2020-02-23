bb7ad56e1e3d5c139c7586055186f0f27216e0eb
stx8991 <49608169+stx8991@users.noreply.github.com>
2020-02-23 19:35:56 +0800

    Add files via upload

---
+++ js/bottom_ads.js
@@ -0,0 +1,4 @@
+//TIPS 设置
+$(".play-tips").css("display","flex");
+$("#tips-index-content").html("【公告】<span style='color:#07A5DF;'>9.yemu.sinaapp.com</span>1-100都是网站网址失效直接往上加1收藏以防失联...");
+//bottom ad
