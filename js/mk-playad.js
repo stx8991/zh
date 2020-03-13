9f5aacbf7b048386e4c94fa9fa7d74f575baf137
stx8991 <49608169+stx8991@users.noreply.github.com>
2020-03-13 19:05:32 +0800

    Create mk-playad.js

---
+++ js/mk-playad.js
@@ -0,0 +1,6 @@
+//ad1
+document.writeln("");
+
+//TIPS 设置
+$(".play-tips").css("display","flex");
+document.getElementById("tips-content").innerHTML="【提醒】:如遇app无法播放 请使用QQ，UC浏览器（可下载、投屏）浏览器打开mkjuba.applinzi.com(你品、你细品)";
