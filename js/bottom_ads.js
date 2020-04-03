2a9f5e1b3048a9da73cd06cd4550ccc98fe58ca5
stx8991 <49608169+stx8991@users.noreply.github.com>
2020-04-03 21:19:47 +0800

    Update bottom_ads.js

--- js/bottom_ads.js
+++ js/bottom_ads.js
@@ -1,4 +1,4 @@
 //TIPS 设置
 $(".play-tips").css("display","flex");
-$("#tips-index-content").html("【公告】<span style='color:#07A5DF;'>9.yemu.sinaapp.com</span>1-100都是网站网址失效直接往上加1收藏以防失联...");
+$("#tips-index-content").html("【公告】<span style='color:#07A5DF;'>为深切哀悼抗击新冠肺炎疫情斗争牺牲烈士和逝世同胞，响应全国哀悼日,2020年4月4日我们对所有参加抗疫的工作人员致以最崇高的敬意。");
 //bottom ad
